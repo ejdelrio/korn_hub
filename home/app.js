4e61370a3877db50e33a58c9088172a5cde7bfec
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-05-27 13:11:07 -0700

    Redid dropdown menu

--- home/app.js
+++ home/app.js
@@ -1,1 +1,33 @@
 
+var menu = document.getElementsByClassName('menu')[0];
+
+function toggle() {
+  var dropDown = document.getElementsByClassName('dropdown')[0];
+  if (dropDown.style.display === 'inline') {
+    hide();
+  } else {
+    unHide();
+  }
+}
+
+function hide() {
+  var dropDown = document.getElementsByClassName('dropdown')[0];
+  var container = document.getElementsByClassName('container');
+  dropDown.style.display = 'none';
+  for (var i = 0; i < container.length; i++) {
+    container[i].style.width = '1071px';
+  }
+}
+
+function unHide() {
+  var dropDown = document.getElementsByClassName('dropdown')[0];
+  var container = document.getElementsByClassName('container');
+  dropDown.style.display = 'inline';
+  for (var i = 0; i < container.length; i++) {
+    container[i].style.width = '860px';
+  }
+}
+
+
+
+menu.addEventListener('click', toggle);
